d1352b7e0e5fae3abca6e638d9edb60800886d94
Josh Parrett <jtparrett@me.com>
2020-07-29 09:43:19 +0100

    export from named component file

---
+++ src/components/Card/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box, Skeleton, Flex, Text } from "@chakra-ui/core";
+
+export const Card = ({ children }) => (
+  <Box borderWidth="1px" rounded="md" overflow="hidden" p={4} as="article">
+    {children}
+  </Box>
+);
+
+const CardStat = ({ name, value, icon, isLoading }) => (
+  <Skeleton height="24px" isLoaded={!isLoading}>
+    <Flex alignItems="center" color="gray.600">
+      <Text mr="auto">{name}</Text>
+      <Text fontWeight="bold">{value}</Text>
+      {icon && <Box as={icon} size="20px" ml={1} />}
+    </Flex>
+  </Skeleton>
+);
+
+CardStat.defaultProps = {
+  name: "",
+  value: "",
+  icon: null,
+  isLoading: false,
+};
+
+CardStat.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.string,
+  icon: PropTypes.any,
+  isLoading: PropTypes.bool,
+};
+
+Card.Stat = CardStat;
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,8 +1,1 @@
-import React from "react";
-import { Box } from "@chakra-ui/core";
-
-export const Wrapper = ({ children, ...props }) => (
-  <Box w={1100} maxW="100%" px={4} mx="auto" {...props}>
-    {children}
-  </Box>
-);
+export * from "./Wrapper";
